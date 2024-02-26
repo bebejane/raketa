f8b3b241c0903e4de21f566742101b1d3cca7420
konst-teknik <information@konst-teknik.se>
2024-02-26 16:34:53 +0100

    Fix

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#ff7a00',
+    theme_color: '#ffffff',
     icons: [
       {
         src: '/favicon.ico',
