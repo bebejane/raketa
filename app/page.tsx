e3007c3d2dbc14adb18d03a3cbca74caadd4801a
konst-teknik <information@konst-teknik.se>
2024-02-26 09:55:05 +0100

    Desktop

--- app/page.tsx
+++ app/page.tsx
@@ -18,19 +18,49 @@ export default async function Start() {
 
   return (
     <>
+      <header className={s.header}>
+        <div className={s.top}>
+          <h1>RAKETA</h1>
+          <a>Contact</a>
+        </div>
+        <div className={s.intro}>
+          <p>
+            RAKETA is running as an ongoing experiment; a laboratory-in-progress – working interdisciplinary, in theory and practice, with projects in the area between art/architecture/philosophy/democracy. By creating alternative meeting places – spaces for talks, thoughts, discussions, workshops, and the unexpected, Raketa poses questions about the world and everything around us. The most important question being – how can we live together?
+            Read more ›
+          </p>
+        </div>
+      </header>
       <article className={cn(s.start)}>
-        <ul>
+        <nav>PROJECTS
+          <ul>
+            <li>Volna.stream</li>
+            <li>Inverted spacest</li>
+            <li>Counterpoint</li>
+          </ul>
+          <div className={s.fade}><div className={s.gradient}></div><div className={s.solid}></div></div>
+        </nav>
+        <ul className={s.projects}>
           {allProjects?.map(({ id, title, description, collaborationWith, externalLink, year, visualPresentation }, idx) =>
             <li key={id}>
-              <h2>{title}</h2>
-              {year}
-              <h3>{collaborationWith}</h3>
-              <Content content={description} />
-              {externalLink && <a href={externalLink}>{externalLink}</a>}
-              <div className={s.presentation}>
+              <div className={s.meta}>
+                <div className={s.wrapper}>
+                  <h2>{title}</h2>
+                  {year}
+                  <h3>{collaborationWith}</h3>
+                  {externalLink && <a href={externalLink}>{externalLink}</a>}
+                </div>
+                <div className={s.fade}><div className={s.gradient}></div><div className={s.solid}></div></div>
+
+              </div>
+              <div className={s.images}>
                 {visualPresentation.map((block, idx) =>
                   <Block key={idx} data={block} components={BlockComponets} />
                 )}
+              </div>
+              <div className={s.text}>
+                <Content content={description} />
+                <div className={s.fade}><div className={s.gradient}></div><div className={s.solid}></div></div>
+
               </div>
             </li>
           )}
