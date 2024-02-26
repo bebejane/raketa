896d1f851e2a8299d285fa075905d8affebda9e9
Bebe Jane <bjornthief@gmail.com>
2024-02-26 10:49:45 +0100

    stuff

--- @types/document-modules.d.ts
+++ @types/document-modules.d.ts
@@ -72,3 +72,12 @@ export const SiteFragment: DocumentNode;
   export default defaultDocument;
 }
     
+
+declare module '*/start.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Start: DocumentNode;
+
+  export default defaultDocument;
+}
+    
